fdbab8d532d9ebab241166af3d26ace1ae629316
JaydenKing32 <jayden.king@students.mq.edu.au>
2020-11-06 02:29:17 +1100

    convert_classes now actually changes the class IDs in the image-specific annotations

--- scripts/convert_classes.py
+++ scripts/convert_classes.py
@@ -15,8 +15,10 @@ with open(source_path, mode='r', encoding="UTF-8") as source_file:
     source_cats = {cat["id"]: cat["name"] for cat in json.load(source_file)["categories"]}
 
 with open(dest_path, mode='r', encoding="UTF-8") as dest_file:
-    dest_cats = {cat["id"]: cat["name"] for cat in json.load(dest_file)["categories"]}
+    copy(dest_path, "{}.bak".format(dest_path))  # Make backup of dest_file
+    dest_json = json.load(dest_file)
 
+dest_cats = {cat["id"]: cat["name"] for cat in dest_json["categories"]}
 new_dest_cats = deepcopy(dest_cats)
 
 for cid, cat in source_cats.items():
@@ -37,16 +39,20 @@ for cid, cat in source_cats.items():
 #         new_dest_cats[i] = source_cats[i]
 
 output_cat_dicts = []
-
 for cid, cat in new_dest_cats.items():
     output_cat_dicts.append({"supercategory": "none", "id": cid, "name": cat})
 
 output_cat_dicts.sort(key=lambda cat: cat["id"])
+dest_json["categories"] = output_cat_dicts
 
-with open(dest_path, mode='r', encoding="UTF-8") as dest_file:
-    copy(dest_path, "{}.bak".format(dest_path))  # Make backup of dest_file
-    dest_json = json.load(dest_file)
-    dest_json["categories"] = output_cat_dicts
+cat_map = {}
+for cid, cat in dest_cats.items():
+    orig_cid = list(new_dest_cats.keys())[list(new_dest_cats.values()).index(cat)]
+    cat_map[cid] = orig_cid
+
+for annotation in dest_json["annotations"]:
+    orig_cid = annotation["category_id"]
+    annotation["category_id"] = cat_map[orig_cid]
 
 with open(dest_path, mode='w', encoding="UTF-8", newline='\n') as dest_file:
     json.dump(dest_json, dest_file)
